34d9c5c3afe83af87a4b18c28901d520ce048806
Ayandarrrr <fezaniayanda@gmail.com>
2026-07-24 10:11:55 +0200

    updated the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import "./App.css";
+
 import SearchBar from "./components/SearchBar";
 import LeagueCard from "./components/LeagueCard";
 import TeamCard from "./components/TeamCard";
+
 import useLeagues from "./hooks/useLeagues";
 import useTeams from "./hooks/useTeams";
 
@@ -27,13 +29,13 @@ function App() {
         setSearchTerm={setSearchTerm}
       />
 
-      {loading && <p>Loading leagues...</p>}
+      {loading && <h2>Loading leagues...</h2>}
 
       {error && <p>{error}</p>}
 
       {!loading && !error && (
         <>
-          <p>Leagues Found: {leagues.length}</p>
+          <h3>Leagues Found: {leagues.length}</h3>
 
           <div className="league-container">
             {leagues.map((league) => (
@@ -44,30 +46,40 @@ function App() {
               />
             ))}
           </div>
+        </>
+      )}
 
-          {selectedLeague && (
+      {selectedLeague && (
+        <>
+          <hr />
+
+          <h2>{selectedLeague}</h2>
+
+          <button
+            className="view-btn"
+            onClick={() => setSelectedLeague("")}
+          >
+            ← Back to Leagues
+          </button>
+
+          {teamsLoading && <h3>Loading teams...</h3>}
+
+          {teamsError && <p>{teamsError}</p>}
+
+          {!teamsLoading && !teamsError && (
             <>
-              <button
-                className="view-btn"
-                onClick={() => setSelectedLeague("")}
-              >
-                ← Back to Leagues
-              </button>
-
-              <h2>{selectedLeague} Teams</h2>
-
-              {teamsLoading && <p>Loading teams...</p>}
-
-              {teamsError && <p>{teamsError}</p>}
-
-              <div className="league-container">
-                {teams.map((team) => (
-                  <TeamCard
-                    key={team.idTeam}
-                    team={team}
-                  />
-                ))}
-              </div>
+              {teams.length > 0 ? (
+                <div className="league-container">
+                  {teams.map((team) => (
+                    <TeamCard
+                      key={team.idTeam}
+                      team={team}
+                    />
+                  ))}
+                </div>
+              ) : (
+                <h3>No teams found for this league.</h3>
+              )}
             </>
           )}
         </>
